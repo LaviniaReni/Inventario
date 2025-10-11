5f6f65edf03e9d2fbd7b400372e4b7c05bcf54a3
LaviniaReni <laviniareni232414@gmail.com>
2025-10-11 20:58:52 -0300

    Update plugin.gd
    se añadio hotbar al plugin

--- addons/inventory_system/plugin.gd
+++ addons/inventory_system/plugin.gd
@@ -4,8 +4,12 @@ extends EditorPlugin
 func _enter_tree():
 	add_custom_type("InventoryManager", "Node", preload("res://addons/inventory_system/scripts/inventory.gd"), preload("res://icon.svg"))
 	add_custom_type("InventoryUI", "Control", preload("res://addons/inventory_system/scripts/inventory_ui.gd"), preload("res://icon.svg"))
-	print("✓ Inventory System activado")
+	add_custom_type("Hotbar", "Node", preload("res://addons/inventory_system/scripts/hotbar.gd"), preload("res://icon.svg"))
+	add_custom_type("HotbarUI", "Control", preload("res://addons/inventory_system/scripts/hotbar_ui.gd"), preload("res://icon.svg"))
+	print("✓ Inventory System activado (con Hotbar)")
 
 func _exit_tree():
 	remove_custom_type("InventoryManager")
 	remove_custom_type("InventoryUI")
+	remove_custom_type("Hotbar")
+	remove_custom_type("HotbarUI")
